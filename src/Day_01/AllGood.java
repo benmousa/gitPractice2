0bb05c872f3a025c4c3a5288600b00153a3eb1b2
benmousa <64154093+benmousa@users.noreply.github.com>
2020-05-28 10:58:03 -0400

    happy Eid

--- src/Day_01/AllGood.java
+++ src/Day_01/AllGood.java
@@ -4,5 +4,8 @@ public class AllGood {
     public static void main(String[] args) {
 
         // I hope everything is GOOD !
+        
+                System.out.println("Happy Birthday again, this line is added from Github");
+
     }
 }

b8611f628ce453703e12fb35a52b3d67259c9029
Mohammed Ali <moh_benmousa@yahoo.com>
2020-05-28 10:44:47 -0400

    Happy Birthday

---
+++ src/Day_01/AddComment.java
@@ -0,0 +1,10 @@
+package Day_01;
+
+public class AddComment {
+    public static void main(String[] args) {
+
+        // I want to try ;
+
+        // i made change
+    }
+}
---
+++ src/Day_01/HappyBirthDay.java
@@ -0,0 +1,8 @@
+package Day_01;
+
+public class HappyBirthDay {
+    public static void main(String[] args) {
+
+        System.out.println("Happy Birthday B18");
+    }
+}
